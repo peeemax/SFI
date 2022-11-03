a122d723ddc5e94a5aecd35fbec5c840246627eb
peeemax <peeemax@hotmail.com>
2022-11-03 16:51:14 -0300

    Configurado template home com front-end bootstrap

--- base/views.py
+++ base/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')
