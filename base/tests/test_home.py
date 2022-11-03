c022e470f7164dd5ae981d0ac3414d50de37df42
peeemax <peeemax@hotmail.com>
2022-11-03 17:29:26 -0300

    Criado e configurado testes para o frontend

--- base/tests/test_home.py
+++ base/tests/test_home.py
@@ -1,5 +1,11 @@
 from django.test import Client
+from SFI.django_assertions import assert_contains
 
 def test_status_code(client:Client):
     resp = client.get('/')
     assert resp.status_code == 200
+    
+def test_title(client:Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>Sistema Famílias Igreja</title>')
+    
