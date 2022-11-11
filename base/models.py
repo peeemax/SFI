e1897d9bb6f40f9c076d2953602058c902a8822d
peeemax <peeemax@hotmail.com>
2022-11-11 16:23:37 -0300

    Criado classe Morador

--- base/models.py
+++ base/models.py
@@ -91,3 +91,48 @@ class User(AbstractUser, PermissionsMixin):
         Send an email to this user.
         """
         send_mail(subject, message, from_email, [self.email], **kwargs)
+
+class Morador(models.Model):
+    
+    DOCS = (
+        ('RG', 'Registro Geral'),
+        ('CPF', 'Cadastro de Pessoa Física'),
+    )
+    
+    TRABALHA = (
+        ('Sim', 'Trabalha'),
+        ('Não', 'Não Trabalha'),
+    )
+    
+    ESTUDA = (
+        ('Sim', 'Estuda'),
+        ('Não', 'Não Estuda'),
+    )
+    
+    SEXOS = (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+    )
+    
+    PARENTESCO = (
+        ('Esposo/a', 'Esposo/a'),
+        ('Filho/a', 'Filho/a'),
+        ('Irmão/ã', 'Filho/ã'),
+        ('Avô/ó', 'Avô/ó'),
+        ('Sobrinho/a', 'Sobrinho/a'),
+        ('Neto/a', 'Neto/a'),
+        ('Tio/a', 'Tio/a'),
+        ('Primo/a', 'Primo/a'),
+        ('Genro/Nora', 'Genro/Nora'),
+        ('Outros', 'Outros')
+    )
+    
+    nome_completo = models.CharField('Nome Completo', max_length=60)
+    documento = models.CharField('Tipo de documento', max_length=10, choices=DOCS)
+    n_documento = models.CharField('Número do documento', max_length=60)
+    data_nascimento = models.DateTimeField('Data de nascimento')
+    trabalhando = models.CharField('Sim ou Não', max_length=20, choices=TRABALHA)
+    estudando = models.CharField('Tipo de documento', max_length=10, choices=ESTUDA)
+    sexo = models.CharField('Tipo de documento', max_length=10, choices=SEXOS)
+    grau_parentesco = models.CharField('Tipo de documento', max_length=10, choices=PARENTESCO)
+    
