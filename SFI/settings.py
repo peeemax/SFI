d8cf5b6056932093e1b0ae464f957ccec4088bfc
peeemax <peeemax@hotmail.com>
2022-12-16 16:37:45 -0300

    Teste deploy settings

--- SFI/settings.py
+++ SFI/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "6b3s31zh!oo5h%r2$k0%v9$w%6nbmo@otydyiou9a$*wee$^+z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = ['*']
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
