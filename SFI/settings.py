05f5f5fa157ff1e447305eac497d26fb697b5dba
peeemax <peeemax@hotmail.com>
2022-12-16 16:35:51 -0300

    Testando deploy

--- SFI/settings.py
+++ SFI/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "6b3s31zh!oo5h%r2$k0%v9$w%6nbmo@otydyiou9a$*wee$^+z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
